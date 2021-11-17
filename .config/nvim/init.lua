c3ae5b79a2cf30b83f554a8197148224584753e1
jacob-ethan <joeyfrio.jf@gmail.com>
2021-11-17 20:55:37 +0800

    switch from init.vim to init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,102 @@
+-- plugins
+require('impatient')
+require('packer').startup(function(use)
+  use "wbthomason/packer.nvim"
+  use "lewis6991/impatient.nvim"
+  use "tpope/vim-surround"
+  use "tpope/vim-commentary"
+  use "tpope/vim-repeat"
+  use "romainl/vim-cool"
+  use "tmsvg/pear-tree"
+  use "lervag/vimtex"
+  use "SirVer/ultisnips"
+  use "pechorin/any-jump.vim"
+  use "jacob-ethan/olivia.vim"
+  -- use 'lifepillar/vim-colortemplate'
+end)
+
+
+-- nvim settings
+vim.cmd('filetype plugin indent on')
+vim.opt.clipboard = vim.opt.clipboard + "unnamedplus"
+vim.opt.backspace = "indent,eol,start"
+vim.opt.omnifunc = "syntaxcomplete#Complete"
+vim.opt.hidden = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.ruler = false
+vim.opt.showmode = false
+vim.opt.showcmd = false
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.wildcharm = vim.fn.char2nr("^Z")
+vim.opt.cmdheight = 1
+vim.opt.scrolloff = 5
+vim.opt.shortmess = "at"
+vim.opt.laststatus = 0
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.shiftround = true
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("config") .. "/undodir"
+vim.opt.completeopt = "menuone,noinsert,noselect"
+vim.opt.shortmess = vim.opt.shortmess + "c"
+vim.opt.conceallevel = 2
+vim.opt.title = true
+
+
+-- mappings
+-- Helper function
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Make Y work like D or C
+map("n", "Y", "y$")
+
+-- Buffer switching
+map("n", "gb", ":buffer *")
+
+-- Quick search/replace
+map("n", "<Space>ip", [[:'{,'}s/\<<C-r>=expand('<cword>')<CR>\>/]])
+map("n", "<Space><Space>", [[:%s/\<<C-r>=expand('<cword>')<CR>\>/]])
+
+-- Quick change
+map("n", "cn", [[/\<<C-R>=expand('<cword>')<CR>\>\C<CR>``cgn]])
+map("n", "cN", [[?\<<C-R>=expand('<cword>')<CR>\>\C<CR>``cgN]])
+
+-- Better j and k
+map("n", "j", "v:count ? 'j' : 'gj'", {expr = true})
+map("n", "k", "v:count ? 'k' : 'gk'", {expr = true})
+
+
+-- autocommands
+-- Return to last edit position when opening files
+vim.cmd [[
+  autocmd BufReadPost *
+       \ if line("'\"") > 0 && line("'\"") <= line("$") |
+       \   exe "normal! g`\"" |
+       \ endif
+]]
+
+-- Disable auto commenting on newlines
+vim.cmd [[
+  autocmd FileType * set formatoptions-=c formatoptions-=r formatoptions-=o
+]]
+
+-- Change line numbers depending on vim mode
+vim.cmd [[
+  autocmd InsertEnter * :set norelativenumber
+  autocmd InsertLeave * :set relativenumber
+]]
+
+
+-- colorscheme
+vim.opt.termguicolors = true
+vim.cmd("colorscheme olivia")
