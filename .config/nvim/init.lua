155f19aa2cd45e4627cc56d17668d72bdbaa7dbc
jacob-ethan <joeyfrio.jf@gmail.com>
2021-11-18 14:30:56 +0800

    nvim-lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,6 @@
 -- plugins
-require("impatient")
+-- packer.nvim
+require("impatient").enable_profile()
 require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
   use "lewis6991/impatient.nvim"
@@ -8,17 +9,48 @@ require("packer").startup(function(use)
   use "tpope/vim-commentary"
   use "tpope/vim-repeat"
   use "romainl/vim-cool"
-  use {"neoclide/coc.nvim", branch = "release"}
+  use "neovim/nvim-lspconfig"
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'L3MON4D3/LuaSnip'
   use "jacob-ethan/olivia.vim"
 end)
 
+-- disable built in plugins
+local disabled_built_ins = {
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "gzip",
+    "zip",
+    "zipPlugin",
+    "tar",
+    "tarPlugin",
+    "getscript",
+    "getscriptPlugin",
+    "vimball",
+    "vimballPlugin",
+    "2html_plugin",
+    "logipat",
+    "rrhelper",
+    "spellfile_plugin",
+    "matchit"
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+    vim.g["loaded_" .. plugin] = 1
+end
+
 
 -- nvim settings
-vim.cmd('filetype plugin indent on')
+vim.cmd("filetype plugin indent on")
 vim.opt.clipboard = vim.opt.clipboard + "unnamedplus"
 vim.opt.backspace = "indent,eol,start"
-vim.opt.omnifunc = "syntaxcomplete#Complete"
+-- vim.opt.omnifunc = "syntaxcomplete#Complete"
 vim.opt.hidden = true
+vim.opt.lazyredraw = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.ruler = false
@@ -46,35 +78,88 @@ vim.opt.conceallevel = 2
 vim.opt.title = true
 
 
+-- plugin settings
+-- lsp stuff
+require("lspconfig").pyright.setup{}
+
+-- nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local luasnip = require 'luasnip'
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
+
+
 -- mappings
--- Helper function
+-- helper function
 local function map(mode, lhs, rhs, opts)
   local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
+  if opts then options = vim.tbl_extend("force", options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
--- Make Y work like D or C
+-- make Y work like D or C
 map("n", "Y", "y$")
 
--- Buffer switching
+-- buffer switching
 map("n", "gb", ":buffer *")
 
--- Quick search/replace
+-- quick search/replace
 map("n", "<Space>ip", [[:'{,'}s/\<<C-r>=expand('<cword>')<CR>\>/]])
 map("n", "<Space><Space>", [[:%s/\<<C-r>=expand('<cword>')<CR>\>/]])
 
--- Quick change
+-- quick change
 map("n", "cn", [[/\<<C-R>=expand('<cword>')<CR>\>\C<CR>``cgn]])
 map("n", "cN", [[?\<<C-R>=expand('<cword>')<CR>\>\C<CR>``cgN]])
 
--- Better j and k
+-- better j and k
 map("n", "j", "v:count ? 'j' : 'gj'", {expr = true})
 map("n", "k", "v:count ? 'k' : 'gk'", {expr = true})
 
 
 -- autocommands
--- Return to last edit position when opening files
+-- return to last edit position when opening files
 vim.cmd [[
   autocmd BufReadPost *
        \ if line("'\"") > 0 && line("'\"") <= line("$") |
@@ -82,12 +167,12 @@ vim.cmd [[
        \ endif
 ]]
 
--- Disable auto commenting on newlines
+-- disable auto commenting on newlines
 vim.cmd [[
   autocmd FileType * set formatoptions-=c formatoptions-=r formatoptions-=o
 ]]
 
--- Change line numbers depending on vim mode
+-- change line numbers depending on vim mode
 vim.cmd [[
   autocmd InsertEnter * :set norelativenumber
   autocmd InsertLeave * :set relativenumber
