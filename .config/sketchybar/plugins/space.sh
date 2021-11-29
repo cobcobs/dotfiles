d3c0aa994d2cb7c9dfcf7b2eb864f46d53ebeb33
jacob-ethan <joeyfrio.jf@gmail.com>
2021-11-29 15:22:19 +0800

    add: space script for bar

---
+++ .config/sketchybar/plugins/space.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+if [ "$SELECTED" = "true" ]; then
+  sketchybar -m --set $NAME icon.highlight=on
+else
+  sketchybar -m --set $NAME icon.highlight=off
+fi
+
+echo $SELECTED
+
