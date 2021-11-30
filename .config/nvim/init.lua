23c1377cc04e00f503ff00d0c4973687609051e5
Jacob <joeyfrio.jf@gmail.com>
2021-11-30 22:11:05 +0800

    slight rewrites (change comments, change tabs to spaces)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,11 @@
+-- packer.nvim bootstrap
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
---- startup and add configure plugins
+-- startup and add plugins
 require("packer").startup(function(use)
   use {"wbthomason/packer.nvim"}
   use {"lewis6991/impatient.nvim"}
