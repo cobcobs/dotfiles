01db628b375c693c3bf5371a53a096e177dc852e
Jacob <joeyfrio.jf@gmail.com>
2022-02-13 23:40:49 +0800

    a lot more LunarVim changes

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,7 +1,37 @@
 -- Additional Plugins
 lvim.plugins = {
-    {"projekt0n/github-nvim-theme"},
-    {"romainl/vim-cool", event = "CmdlineEnter"},
+	{ "projekt0n/github-nvim-theme" },
+	{ "romainl/vim-cool", event = "CmdlineEnter" },
+	{ "tpope/vim-repeat" },
+	{ "tpope/vim-surround" },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "BufRead",
+		setup = function()
+			vim.g.indentLine_enabled = 1
+			vim.g.indent_blankline_char = "▏"
+			vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard" }
+			vim.g.indent_blankline_buftype_exclude = { "terminal" }
+			vim.g.indent_blankline_show_trailing_blankline_indent = false
+			vim.g.indent_blankline_show_first_indent_level = false
+		end,
+	},
+	{
+		"ethanholz/nvim-lastplace",
+		event = "BufRead",
+		config = function()
+			require("nvim-lastplace").setup({
+				lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+				lastplace_ignore_filetype = {
+					"gitcommit",
+					"gitrebase",
+					"svn",
+					"hgcommit",
+				},
+				lastplace_open_folds = true,
+			})
+		end,
+	},
 }
 
 -- general vim settings
@@ -14,6 +44,7 @@ vim.opt.wrap = true
 -- general lvim settings
 lvim.log.level = "warn"
 lvim.format_on_save = true
+lvim.lsp.diagnostics.virtual_text = false
 lvim.colorscheme = "github_light"
 lvim.transparent_window = true
 
@@ -24,15 +55,16 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<C-p>"] = ":Telescope<cr>"
 lvim.keys.normal_mode["<C-f>"] = ":Telescope current_buffer_fuzzy_find<cr>"
 lvim.keys.normal_mode["<C-n>"] = ":NvimTreeToggle<cr>"
-lvim.keys.normal_mode["j"] = {"v:count ? 'j' : 'gj'", {expr = true}}
-lvim.keys.normal_mode["k"] = {"v:count ? 'k' : 'gk'", {expr = true}}
+lvim.keys.normal_mode["j"] = { "v:count ? 'j' : 'gj'", { expr = true } }
+lvim.keys.normal_mode["k"] = { "v:count ? 'k' : 'gk'", { expr = true } }
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 -- enable/disable plugins
+lvim.builtin.bufferline.active = true
 lvim.builtin.dashboard.active = true
+lvim.builtin.lualine.active = false
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
-lvim.builtin.lualine.active = false
 
 -- nvim-tree
 lvim.builtin.nvimtree.setup.view.side = "left"
@@ -40,89 +72,58 @@ lvim.builtin.nvimtree.show_icons.git = 0
 
 -- gitsigns
 lvim.builtin.gitsigns.opts = {
-  yadm = {
-    enable = true
-  }
+	yadm = {
+		enable = true,
+	},
 }
 
 -- nvim-cmp
 lvim.builtin.cmp.sources = {
-  {name = "luasnip"},
-  {name = "nvim_lsp"},
-  {name = "nvim_lua"},
-  {name = "latex_symbols"},
-  {name = "path"},
-  {name = "buffer"},
+	{ name = "luasnip" },
+	{ name = "nvim_lsp" },
+	{ name = "nvim_lua" },
+	{ name = "latex_symbols" },
+	{ name = "path" },
+	{ name = "buffer" },
 }
 
 -- telescope.nvim
 lvim.builtin.telescope.pickers = {
-  find_files = {
-    find_command = {"fd", "--hidden"},
-  },
-  builtin = {
-    theme = "dropdown"
-  }
+	find_files = {
+		find_command = { "fd", "--hidden" },
+	},
+	builtin = {
+		theme = "dropdown",
+	},
 }
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+	"bash",
+	"c",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"typescript",
+	"tsx",
+	"css",
+	"rust",
+	"java",
+	"yaml",
 }
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- generic LSP settings
--- set a formatter, this will override the language server formatting capabilities (if it exists)
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { command = "black", filetypes = { "python" } },
-  { command = "isort", filetypes = { "python" } },
-  {
-    command = "prettier",
-    extra_args = { "--print-with", "100" },
-    filetypes = { "typescript", "typescriptreact" },
-  },
-}
-
--- set additional linters
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  { command = "flake8", filetypes = { "python" } },
-  {
-    command = "shellcheck",
-    extra_args = { "--severity", "warning" },
-  },
-  {
-    command = "codespell",
-    filetypes = { "javascript", "python" },
-  },
-}
-
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 lvim.autocommands.custom_groups = {
-  -- return to last edit position when opening files
-  { "BufReadPost", "*", [[if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal! g`\"" | endif]] },
+	-- change line numbers depending on vim mode
+	{ "InsertEnter", "*", ":set norelativenumber" },
+	{ "InsertLeave", "*", ":set relativenumber" },
 
-  -- change line numbers depending on vim mode
-  { "InsertEnter", "*", ":set norelativenumber" },
-  { "InsertLeave", "*", ":set relativenumber" },
+	-- disable auto commenting on newlines
+	{ "FileType", "*", "set formatoptions-=c formatoptions -=r formatoptions -=o" },
 
-  -- disable auto commenting on newlines
-  { "FileType", "*", "set formatoptions-=c formatoptions -=r formatoptions -=o" },
-
-  -- colorscheme
-  { "ColorScheme", "*", ":hi Cursorline ctermbg=none guibg=none"}
+	-- colorscheme
+	{ "ColorScheme", "*", ":hi Cursorline ctermbg=none guibg=none" },
 }
-
