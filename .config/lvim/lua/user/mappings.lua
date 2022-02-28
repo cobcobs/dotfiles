099217b8de349956bb5036c255b2727e2d0dfb0f
Jacob <joeyfrio.jf@gmail.com>
2022-02-28 19:44:45 +0800

    change quick search/replace mappings to which_key mappings

--- .config/lvim/lua/user/mappings.lua
+++ .config/lvim/lua/user/mappings.lua
@@ -6,9 +6,9 @@ M.config = function()
 	lvim.keys.normal_mode["cN"] = [[?\<<C-R>=expand('<cword>')<CR>\>\C<CR>``cgN]]
 	lvim.keys.normal_mode["j"] = { "v:count ? 'j' : 'gj'", { expr = true } }
 	lvim.keys.normal_mode["k"] = { "v:count ? 'k' : 'gk'", { expr = true } }
-	lvim.keys.normal_mode["<Space><Space>"] = { [[:'{,'}s/\<<C-r>=expand('<cword>')<CR>\>/]], { noremap = true } }
-	lvim.keys.normal_mode["<Space>%"] = { [[:%s/\<<C-r>=expand('<cword>')<CR>\>/]], { noremap = true } }
 
+	lvim.builtin.which_key.mappings["rp"] = { [[:'{,'}s/\<<C-r>=expand('<cword>')<CR>\>/]], "Replace in Paragraph" }
+	lvim.builtin.which_key.mappings["ra"] = { [[:%s/\<<C-r>=expand('<cword>')<CR>\>/]], "Replace All" }
 	lvim.builtin.which_key.mappings["f"] = { "<cmd>Telescope find_files<CR>", "Find File" }
 	lvim.builtin.which_key.mappings["/"] = { "<cmd>Telescope current_buffer_fuzzy_find<CR>", "Fuzzy Find" }
 	lvim.builtin.which_key.mappings["c"] = {
