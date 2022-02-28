2127971585af3118a82b1c782e3ea778f4fc390a
Jacob <joeyfrio.jf@gmail.com>
2022-02-28 17:52:29 +0800

    quick search/replace mappings for lunarvim

--- .config/lvim/lua/user/mappings.lua
+++ .config/lvim/lua/user/mappings.lua
@@ -6,6 +6,8 @@ M.config = function()
 	lvim.keys.normal_mode["cN"] = [[?\<<C-R>=expand('<cword>')<CR>\>\C<CR>``cgN]]
 	lvim.keys.normal_mode["j"] = { "v:count ? 'j' : 'gj'", { expr = true } }
 	lvim.keys.normal_mode["k"] = { "v:count ? 'k' : 'gk'", { expr = true } }
+	lvim.keys.normal_mode["<Space><Space>"] = { [[:'{,'}s/\<<C-r>=expand('<cword>')<CR>\>/]], { noremap = true } }
+	lvim.keys.normal_mode["<Space>%"] = { [[:%s/\<<C-r>=expand('<cword>')<CR>\>/]], { noremap = true } }
 
 	lvim.builtin.which_key.mappings["f"] = { "<cmd>Telescope find_files<CR>", "Find File" }
 	lvim.builtin.which_key.mappings["/"] = { "<cmd>Telescope current_buffer_fuzzy_find<CR>", "Fuzzy Find" }
