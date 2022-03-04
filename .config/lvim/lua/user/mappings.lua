cce79d29432d64140369b34a50f2b58ba0249113
Jacob <joeyfrio.jf@gmail.com>
2022-03-04 12:51:31 +0800

    rewrite which-key mappings

--- .config/lvim/lua/user/mappings.lua
+++ .config/lvim/lua/user/mappings.lua
@@ -7,17 +7,23 @@ M.config = function()
 	lvim.keys.normal_mode["j"] = { "v:count ? 'j' : 'gj'", { expr = true } }
 	lvim.keys.normal_mode["k"] = { "v:count ? 'k' : 'gk'", { expr = true } }
 
-	lvim.builtin.which_key.mappings["rp"] = { [[:'{,'}s/<C-r><C-w>//g<Left><Left>]], "Replace in Paragraph" }
-	lvim.builtin.which_key.mappings["ra"] = { [[:%s/<C-r><C-w>//g<Left><Left>]], "Replace All" }
-	lvim.builtin.which_key.mappings["f"] = { "<cmd>Telescope find_files<CR>", "Find File" }
-	lvim.builtin.which_key.mappings["/"] = { "<cmd>Telescope current_buffer_fuzzy_find<CR>", "Fuzzy Find" }
+	lvim.builtin.which_key.mappings["r"] = {
+		name = "Replace",
+		a = { [[:'{,'}s/<C-r><C-w>//g<Left><Left>]], "In Paragraph" },
+		p = { [[:%s/<C-r><C-w>//g<Left><Left>]], "All" },
+	}
+
 	lvim.builtin.which_key.mappings["c"] = {
 		"<cmd>lua require('Comment.api').toggle_current_linewise()<CR>",
 		"Comment",
 	}
+	lvim.builtin.which_key.mappings["f"] = { "<cmd>Telescope find_files<CR>", "Find File" }
+	lvim.builtin.which_key.mappings["n"] = { ":ene <BAR> startinsert <CR>", "New File" }
+	lvim.builtin.which_key.mappings["/"] = { "<cmd>Telescope current_buffer_fuzzy_find<CR>", "Fuzzy Find" }
 	lvim.builtin.which_key.mappings[";"] = { "<cmd>Alpha<CR>", "Dashboard" }
-	lvim.builtin.which_key.mappings["sP"] = { "<cmd>Telescope projects<CR>", "Recent Project" }
 
+	lvim.builtin.which_key.mappings["sP"] = { "<cmd>Telescope projects<CR>", "Recent Project" }
+	lvim.builtin.which_key.mappings["sr"] = { "<cmd>Telescope frecency<CR>", "Recent Files" }
 	lvim.builtin.which_key.mappings["bd"] = { "<cmd>BufferKill<CR>", "Delete Buffer" }
 	lvim.builtin.which_key.mappings["Ls"] = {
 		":source ~/.config/lvim/config.lua<CR>",
