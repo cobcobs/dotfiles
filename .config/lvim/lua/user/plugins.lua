0974d8e27ae37bd032b52457954f077426e615b9
Jacob <joeyfrio.jf@gmail.com>
2022-03-07 15:27:38 +0800

    disable indent lines on packer window

--- .config/lvim/lua/user/plugins.lua
+++ .config/lvim/lua/user/plugins.lua
@@ -16,7 +16,7 @@ M.config = function()
 			setup = function()
 				vim.g.indentLine_enabled = 1
 				vim.g.indent_blankline_char = "▏"
-				vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "alpha" }
+				vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "alpha", "packer" }
 				vim.g.indent_blankline_buftype_exclude = { "terminal" }
 				vim.g.indent_blankline_show_trailing_blankline_indent = false
 				vim.g.indent_blankline_show_first_indent_level = false
@@ -83,7 +83,7 @@ M.config = function()
 	greeter.section.footer.val = { "lunarvim.org" }
 
 	vim.g.vimtex_view_method = "zathura"
-	vim.cmd([[
+  vim.cmd([[
     let g:vimtex_compiler_latexmk = {
     \ 'options' : [
     \   '-shell-escape',
