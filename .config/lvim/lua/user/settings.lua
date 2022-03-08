432cbd6070b25947ac5ee3ef44a6ca719e2a0055
Jacob <joeyfrio.jf@gmail.com>
2022-03-08 11:40:22 +0800

    disable filetype.lua for now

--- .config/lvim/lua/user/settings.lua
+++ .config/lvim/lua/user/settings.lua
@@ -8,8 +8,8 @@ M.config = function()
 	vim.opt.wrap = true
 	vim.opt.relativenumber = true
 	vim.opt.cursorline = false
-  vim.g.do_filetype_lua = 1
-  vim.g.did_load_filetypes = 0
+  -- vim.g.do_filetype_lua = 1
+  -- vim.g.did_load_filetypes = 0
 
 	lvim.leader = "space"
 	lvim.log.level = "warn"
