841e45daaec931930f95d8a8d46c05b95c7aba9e
Jacob <joeyfrio.jf@gmail.com>
2022-03-09 11:26:04 +0800

    use termpdf.py with vimtex

--- .config/lvim/lua/user/plugins.lua
+++ .config/lvim/lua/user/plugins.lua
@@ -68,7 +68,8 @@ M.config = function()
     },
   })
 
-	vim.g.vimtex_view_method = "zathura"
+	vim.g.vimtex_view_general_viewer = "kitty"
+	vim.g.vimtex_view_general_options = "--single-instance termpdf.py @pdf"
   vim.cmd([[
     let g:vimtex_compiler_latexmk = {
     \ 'options' : [
