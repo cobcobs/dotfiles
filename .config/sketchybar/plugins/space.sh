9d991e8090c61fcb77e85f2520cf9bdc34d86bd6
Jacob <joeyfrio.jf@gmail.com>
2022-09-23 01:38:23 -0700

    improved bar aesthetics

--- .config/sketchybar/plugins/space.sh
+++ .config/sketchybar/plugins/space.sh
@@ -2,14 +2,16 @@
 
 
 WIN=$(yabai -m query --spaces --space "$SID" | jq '.windows[0]')
-HAS_WINDOWS_OR_IS_SELECTED="true"
-if [ "$WIN" = "null" ] && [ "$SELECTED" = "false" ];then
-  HAS_WINDOWS_OR_IS_SELECTED="false"
-fi
-sketchybar -m --set "$NAME" icon.drawing="$HAS_WINDOWS_OR_IS_SELECTED"
+WIDTH=$(sketchybar --query space.1 | jq '.icon.width')
 
 if [ "$SELECTED" = "true" ]; then
-  sketchybar -m --set "$NAME" icon.highlight=on
+  sketchybar --set "$NAME" icon.drawing=true
+  sketchybar -m --animate tanh 10 --set "$NAME" icon.width="$WIDTH"
+  sketchybar -m --animate tanh 10 --set "$NAME" icon.highlight=off icon.highlight=on
+elif [ "$WIN" = "null" ]; then
+  sketchybar -m --animate tanh 10 --set "$NAME" icon.width=0
+  sleep 0.2
+  sketchybar --set "$NAME" icon.drawing=false
 else
-  sketchybar -m --set "$NAME" icon.highlight=off
+  sketchybar -m --animate tanh 10 --set "$NAME" icon.highlight=on icon.highlight=off
 fi
