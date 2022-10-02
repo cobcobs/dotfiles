bf11bee58ac459d4e5c9b601943fb3ba7d57c50a
Jacob <44166162+cobcobs@users.noreply.github.com>
2022-10-01 21:28:26 -0700

    small sketchybar script fixes

--- .config/sketchybar/plugins/power.sh
+++ .config/sketchybar/plugins/power.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 
-. ~/.cache/wal/colors.sh
 BATT_PERCENT=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
-CHARGING=$(pmset -g batt | grep 'AC Power')
 
 sketchybar -m --set battery\
-  icon_color=$COLOR \
-  icon=\
-  label=$(printf "${BATT_PERCENT}%%")
+  icon=\
+  label="$(printf "${BATT_PERCENT}%%")"
