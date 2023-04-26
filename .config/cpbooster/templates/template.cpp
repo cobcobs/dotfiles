e6c12309db34d716055b833536e534a6fbcd1670
Jacob <44166162+cobcobs@users.noreply.github.com>
2023-04-25 17:13:31 -0700

    change template.cpp to follow google style guide

--- .config/cpbooster/templates/template.cpp
+++ .config/cpbooster/templates/template.cpp
@@ -11,23 +11,20 @@ typedef long unsigned long lul;
 
 const int MOD = 1e9 + 7;
 
-int solve()
-{
-    return 0;
+int solve() {
+  return 0;
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
 
-    bool multiple_testcases = false;
-    int T = 1;
-    if (multiple_testcases)
-        cin >> T;
-    for (int t = 1; t <= T; t++) {
-        // cout << "Case #" << t << ": ";
-        solve();
-    }
-    return 0;
+  bool multiple_testcases = false;
+  int T = 1;
+  if (multiple_testcases) cin >> T;
+  for (int t = 1; t <= T; t++) {
+    // cout << "Case #" << t << ": ";
+    solve();
+  }
+  return 0;
 }
