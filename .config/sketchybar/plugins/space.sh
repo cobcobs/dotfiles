15ec3c16e3883615859d39cdc38fb2accaf96401
Jacob <44166162+cobcobs@users.noreply.github.com>
2023-11-27 19:24:37 -0500

    stuff

--- .config/sketchybar/plugins/space.sh
+++ .config/sketchybar/plugins/space.sh
@@ -1,39 +1,7 @@
 #!/bin/sh
 
-update() {
-	WIDTH="dynamic"
-	if [ "$SELECTED" = "true" ]; then
-		WIDTH="0"
-	fi
 
-	FULLSCREEN=$(yabai -m query --windows --window | jq '.["is-native-fullscreen"]')
-	bar_height=$(sketchybar --query bar | jq '.height')
-	current_offset=$(sketchybar --query bar | jq '.y_offset')
-
-	if [ "$FULLSCREEN" = "true" ]; then
-		launchctl load -F /System/Library/LaunchAgents/com.apple.OSDUIHelper.plist >/dev/null 2>&1 &
-	elif [ "$((-current_offset))" != "$bar_height" ]; then
-		launchctl unload -F /System/Library/LaunchAgents/com.apple.OSDUIHelper.plist >/dev/null 2>&1 &
-	fi
-
-	sketchybar --animate tanh 20 --set $NAME icon.highlight=$SELECTED label.width=$WIDTH
-
-}
-
-mouse_clicked() {
-	if [ "$BUTTON" = "right" ]; then
-		yabai -m space --destroy $SID
-		sketchybar --trigger space_change
-	else
-		yabai -m space --focus $SID 2>/dev/null
-	fi
-}
-
-case "$SENDER" in
-"mouse.clicked")
-	mouse_clicked
-	;;
-*)
-	update
-	;;
+case $NAME in
+  space_indicator) sketchybar --set "$NAME" label="$(yabai -m query --spaces --space | jq -r '.index')";;
+  *) sketchybar --animate tanh 20 --set "$NAME" icon.highlight="$SELECTED";;
 esac
